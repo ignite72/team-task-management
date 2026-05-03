74b15530ae9ada5475abd9037c8f146b3af538a7
ignite72 <vanshrangara925@gmail.com>
2026-05-03 15:43:02 +0530

    fix start script

--- server.js
+++ server.js
@@ -245,6 +245,8 @@ mongoose
   .catch((err) => console.log(" DB ERROR:", err.message));
 
 // 6. start server LAST
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
