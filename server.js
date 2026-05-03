845d9dd6d5f9d6e884b5261faecb4994f04b5490
ignite72 <vanshrangara925@gmail.com>
2026-05-03 18:32:00 +0530

    add root route

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 // 4. routes
 app.post("/signup", async (req, res) => {
   console.log("🔥 SIGNUP ROUTE HIT");
